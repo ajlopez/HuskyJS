e2a0eb81bdbf4c905702960caba54c73f8863643
ajlopez <webmaster@ajlopez.com>
2017-06-24 11:03:10 -0300

    Function type

--- test/types.js
+++ test/types.js
@@ -11,3 +11,13 @@ exports['String as type'] = function (test) {
 	test.ok(types.isType(types.String));
 };
 
+exports['Function type'] = function (test) {
+	var fnt = types.func(types.String, types.Integer);
+	
+	test.ok(fnt);
+	test.ok(types.isType(fnt));
+	test.ok(fnt.from());
+	test.equal(fnt.from(), types.String);
+	test.equal(fnt.to(), types.Integer);
+};
+
