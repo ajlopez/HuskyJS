83d3a100d9adc12faa62ff7bc4b447cb2e68ad99
ajlopez <webmaster@ajlopez.com>
2017-07-11 11:37:36 -0300

    New primitive types

--- test/types.js
+++ test/types.js
@@ -6,9 +6,24 @@ exports['Integer as type'] = function (test) {
 	test.ok(types.isType(types.Integer));
 };
 
+exports['Double as type'] = function (test) {
+	test.ok(types.Double);
+	test.ok(types.isType(types.Double));
+};
+
 exports['String as type'] = function (test) {
 	test.ok(types.String);
 	test.ok(types.isType(types.String));
+};
+
+exports['Bool as type'] = function (test) {
+	test.ok(types.Bool);
+	test.ok(types.isType(types.Bool));
+};
+
+exports['Char as type'] = function (test) {
+	test.ok(types.Char);
+	test.ok(types.isType(types.Char));
 };
 
 exports['Function type'] = function (test) {
