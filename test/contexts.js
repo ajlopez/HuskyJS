15af1c0d8f5a5a38fa5cf3e1c97428e45c9efc34
ajlopez <webmaster@ajlopez.com>
2017-07-12 10:19:44 -0300

    Top context

--- test/contexts.js
+++ test/contexts.js
@@ -1,5 +1,6 @@
 
 var contexts = require('../lib/contexts');
+var types = require('../lib/types');
 
 exports['get undefined value'] = function (test) {
 	var ctx = contexts.context();
@@ -15,4 +16,14 @@ exports['set and get value'] = function (test) {
 	test.equal(ctx.get('foo'), 'bar');
 };
 
+exports['top context'] = function (test) {
+	var ctx = contexts.topContext();
+	
+	test.ok(ctx.get('String'));
+	test.ok(types.isType(ctx.get('String')));
+	test.ok(ctx.get('Integer'));
+	test.ok(types.isType(ctx.get('Integer')));
+	test.ok(ctx.get('->'));
+	test.equal(typeof ctx.get('->'), 'function');
+};
 
