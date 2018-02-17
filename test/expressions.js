a3d1fe93c38525bdb0fcf4e1cb5572d0073fbee9
ajlopez <webmaster@ajlopez.com>
2018-02-17 15:34:35 -0300

    Additional test, evaluate string constant expression

--- test/expressions.js
+++ test/expressions.js
@@ -12,3 +12,14 @@ exports['evaluate integer constant expression'] = function (test) {
 	test.strictEqual(result, 42);
 };
 
+exports['evaluate string constant expression'] = function (test) {
+	var expr = expressions.constant('foo');
+	
+	test.ok(expr);
+	
+	var result = expr.evaluate();
+	
+	test.ok(result);
+	test.strictEqual(result, 'foo');
+};
+
