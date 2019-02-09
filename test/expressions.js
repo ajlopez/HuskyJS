1d32c206e1e5f195297c6a09d296613abb48b6fa
ajlopez <webmaster@ajlopez.com>
2019-02-09 19:40:58 -0300

    Additional test: apply with two arguments

--- test/expressions.js
+++ test/expressions.js
@@ -59,5 +59,14 @@ exports['build apply expression'] = function (test) {
 	test.equal(result, 42);
 };
 
+exports['build apply expression twice'] = function (test) {
+	var ctx = contexts.topContext();
+	var expr = expressions.apply(expressions.apply(expressions.composite(expressions.operator('+'), [ expressions.name('a'), expressions.name('b') ]), 'b'), 'a');
+
+    var result = expr.evaluate(ctx)(40)(2);
+    
+	test.equal(result, 42);
+};
+
 
 
