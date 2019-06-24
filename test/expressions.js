8885ebed821fd7c17ac7d668df7803a414006660
ajlopez <webmaster@ajlopez.com>
2019-06-24 09:14:30 -0300

    Expression tests refactor

--- test/expressions.js
+++ test/expressions.js
@@ -3,43 +3,43 @@ const expressions = require('../lib/expressions');
 const contexts = require('../lib/contexts');
 
 exports['evaluate integer constant expression'] = function (test) {
-	var expr = expressions.constant(42);
+	const expr = expressions.constant(42);
 	
 	test.ok(expr);
 	
-	var result = expr.evaluate();
+	const result = expr.evaluate();
 	
 	test.ok(result);
 	test.strictEqual(result, 42);
 };
 
 exports['evaluate string constant expression'] = function (test) {
-	var expr = expressions.constant('foo');
+	const expr = expressions.constant('foo');
 	
 	test.ok(expr);
 	
-	var result = expr.evaluate();
+	const result = expr.evaluate();
 	
 	test.ok(result);
 	test.strictEqual(result, 'foo');
 };
 
 exports['evaluate name expression'] = function (test) {
-	var ctx = contexts.context();
+	const ctx = contexts.context();
 	ctx.set('answer', 42);
 	
-	var expr = expressions.name('answer');
+	const expr = expressions.name('answer');
 	
 	test.ok(expr);
 	
-	var result = expr.evaluate(ctx);
+	const result = expr.evaluate(ctx);
 	
 	test.ok(result);
 	test.strictEqual(result, 42);
 };
 
 exports['build composite expression'] = function (test) {
-	var expr = expressions.composite(expressions.name('fn'), [ expressions.constant(42) ]);
+	const expr = expressions.composite(expressions.name('fn'), [ expressions.constant(42) ]);
 	
 	test.ok(expr);
 	test.ok(expr.expression()),
@@ -51,26 +51,26 @@ exports['build composite expression'] = function (test) {
 };
 
 exports['build apply expression'] = function (test) {
-	var ctx = contexts.context();
-	var expr = expressions.apply(expressions.name('n'), 'n');
+	const ctx = contexts.context();
+	const expr = expressions.apply(expressions.name('n'), 'n');
 
-    var result = expr.evaluate(ctx)(42);
+    const result = expr.evaluate(ctx)(42);
     
 	test.equal(result, 42);
 };
 
 exports['build apply expression twice'] = function (test) {
-	var ctx = contexts.topContext();
-	var expr = expressions.apply(expressions.apply(expressions.composite(expressions.operator('+'), [ expressions.name('a'), expressions.name('b') ]), 'b'), 'a');
+	const ctx = contexts.topContext();
+	const expr = expressions.apply(expressions.apply(expressions.composite(expressions.operator('+'), [ expressions.name('a'), expressions.name('b') ]), 'b'), 'a');
 
-    var result = expr.evaluate(ctx)(40)(2);
+    const result = expr.evaluate(ctx)(40)(2);
     
 	test.equal(result, 42);
 };
 
 exports['build binary expression'] = function (test) {
-	var ctx = contexts.topContext();
-	var expr = expressions.binary(expressions.name('+'), expressions.constant(40), expressions.constant(2));
+	const ctx = contexts.topContext();
+	const expr = expressions.binary(expressions.name('+'), expressions.constant(40), expressions.constant(2));
 	
 	test.ok(expr);
 
